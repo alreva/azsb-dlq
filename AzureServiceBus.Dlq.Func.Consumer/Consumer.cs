d062bae2bbf629c581d6ff09649d0623b14f6848
oleksandr.reva <alexander.reva@sigma.software>
2022-11-07 19:40:49 +0200

    initial commit

---
+++ AzureServiceBus.Dlq.Func.Consumer/Consumer.cs
@@ -0,0 +1,16 @@
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+
+namespace AzureServiceBus.Dlq.Func.Consumer;
+
+public class Consumer
+{
+    [FunctionName("Consumer")]
+    public void Run(
+        [ServiceBusTrigger("customers", Connection = "")]
+        string myQueueItem,
+        ILogger log)
+    {
+        log.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
+    }
+}
